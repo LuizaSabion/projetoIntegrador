76826332ba310edae97674d5b3703a61e216e802
luiza <marilusabion2203@gmail.com>
2022-06-29 11:52:10 -0300

    index - menu

---
+++ Cadastros/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Principal</title>
+   
+</head>
+<body>
+    <h1>Menu Principal</h1>
+    <div id="menu">
+        <ul>
+            <li><a href= "usuarios\cadastro_usuario.html">1.Cadastro de usuarios</a></li>
+            
+            <li><a href="moveis\cadastro_moveis.html"> 2.Cadastro de moveis</a></li>
+        </ul>
+    </div>
+</body>
+</html>
